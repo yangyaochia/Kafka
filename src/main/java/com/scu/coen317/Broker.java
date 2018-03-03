84e0da5586d8c6043fbf5c30ad4d54afe649efa8
Xinzhu Cai <kongxinzhu.s@gmail.com>
2018-03-02 16:48:19 -0800

    test broker

--- src/main/java/com/scu/coen317/Broker.java
+++ src/main/java/com/scu/coen317/Broker.java
@@ -1,4 +1,22 @@
 package com.scu.coen317;
 
+import java.io.*;
+import java.net.*;
+
 public class Broker {
+    public static void main(String argv[]) throws Exception {
+        String clientSentence;
+        String response;
+        ServerSocket welcomeSocket = new ServerSocket(9000);
+        while (true) {
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            response = "received : " + clientSentence;
+            outToClient.writeBytes(response);
+        }
+    }
 }
+
