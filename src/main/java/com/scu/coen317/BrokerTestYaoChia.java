f3fce456a7cbd375af5416770c5ad886389cff4c
yangyaochia <yangyaochiatw@gmail.com>
2018-03-07 20:06:47 -0800

    Broker Class modify topicMember to topicsPartitionLeader add topicPartitionReplicationBroker

--- src/main/java/com/scu/coen317/BrokerTestYaoChia.java
+++ src/main/java/com/scu/coen317/BrokerTestYaoChia.java
@@ -2,8 +2,9 @@ package com.scu.coen317;
 
 public class BrokerTestYaoChia {
     public static void main(String argv[]) throws Exception {
-        Broker b = new Broker("localhost", 9000, "localhost", 2181);
-        b.listen();
-
+        Broker b1 = new Broker("localhost", 9000, "localhost", 2181);
+        Broker b2 = new Broker("localhost", 9001, "localhost", 2181);
+        b1.listen();
+        b2.listen();
     }
 }
