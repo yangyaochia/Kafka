76f90817a649878b60f31d300a9004e41ea5c915
Xinzhu Cai <kongxinzhu.s@gmail.com>
2018-03-10 22:09:33 -0800

    add consumerTest

---
+++ src/main/java/com/scu/coen317/TestConsumer1.java
@@ -0,0 +1,32 @@
+package com.scu.coen317;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.ExecutorService;
+
+public class TestConsumer1 {
+    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, IOException, InterruptedException {
+        Consumer con1 = new Consumer("localhost", 10001, "group1", "localhost", 9005);
+        con1.findCoordinator();
+//        con1.updateCoordinator(new HostRecord("localhost", 9005));
+        con1.joinToGroup();
+        con1.subscribe("topic1");
+
+
+        /*Consumer consumer = new Consumer("localhost", 10001, "group1", "localhost", 9005);
+        consumer.initialLeader();
+        Message request = new Message(MessageType.TEST1);
+//        // send request to defaultBroker with the groupId
+        try {
+            TcpClient sock = new TcpClient("localhost", 9004);
+            sock.setHandler(consumer, request);
+            sock.run();
+        } catch (Exception e) {
+//            e.printStackTrace();
+            TcpClient sock = new TcpClient("localhost", 9005);
+            sock.setHandler(consumer, request);
+            sock.run();
+        }
+*/
+    }
+}
