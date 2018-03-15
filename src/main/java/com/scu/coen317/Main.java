89b13ab708b0cf8a660592c0d2253d0ddf6a53ec
kongxinzhu <kongxinzhu.s@gmail.com>
2018-03-14 18:23:22 -0700

    Update Main.java

--- src/main/java/com/scu/coen317/Main.java
+++ src/main/java/com/scu/coen317/Main.java
@@ -7,17 +7,12 @@ import com.scu.coen317.Broker;
 
 public class Main {
     public static void main(String[] args) {
-        // Broker localhost 9000 localhost 2181
-        // Consumer localhost 10001 group1 localhost 9005
-        // Producer localhost 8000 localhost 9000 2 2
-        // Zookeeper localhost 2181
-
         String host = "localhost";
         int portNumber = Integer.parseInt(args[2]);
         int defautPort = 9005;
         if (args.length < 3) {
             System.out.println("Error : Illegal input.");
-        } else
+        } else {
             try {
                 if (args[0].equals("Zookeeper")) {
                     Zookeeper zookeeper = new Zookeeper(host, portNumber);
@@ -39,18 +34,12 @@ public class Main {
                     for (int i = 0; i < 1500; i++) {
                         p.publishMessage(topic, "Topic: Message from " +args[2]+ " " + Integer.toString(i));
                     }
-//                    p.createTopic("Distributed System Topic", 3,1);
-//                    for ( int i = 0 ; i < 1500 ; i++) {
-//                        p.publishMessage(topic, "DS Topic: Message from 8000 " + Integer.toString(i));
-//                    }
                 } else if (args[0].equals("Consumer")) {
-
                     String topic1 = args[6];
                     System.out.println(topic1);
                     String topic2 = args.length == 8 ? args[7] : "";
                     Consumer consumer = new Consumer(host, Integer.parseInt(args[2]),
                             args[3], host, Integer.parseInt(args[5]));
-
                     consumer.findCoordinator();
                     consumer.joinToGroup();
                     consumer.subscribe(topic1);
@@ -58,18 +47,6 @@ public class Main {
                         consumer.subscribe(topic2);
                     }
                     consumer.poll();
-
-
-//                    Consumer con1 = new Consumer("localhost", 10001, "group1",
-//                            "localhost", 9000);
-//                    con1.findCoordinator();
-////        con1.updateCoordinator(new HostRecord("localhost", 9005));
-//                    con1.joinToGroup();
-//                    con1.subscribe("DS");
-////        Thread.sleep(1000);
-//                    con1.poll();
-
-
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -89,6 +66,6 @@ public class Main {
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
+        }
     }
-
 }
